01d1f073e4e727e4d8b9d96fbfbe8b778b3f76f1
kanishk6103 <mamta11tiwari06@gmail.com>
2023-03-28 02:00:38 +0530

    Responsive hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    // screens: {
+    //   'desktop': '1280px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
   },
   plugins: [],
 };
